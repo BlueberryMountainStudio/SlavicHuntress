e67f53a232134c37b317de9b967798cd65f92dd4
Tyrannanas <129948998+SimonBinzenbach@users.noreply.github.com>
2023-08-08 16:51:07 +0200

    base classes v1
    Foe Base Class added 2 test foes added working properly
    main character base class added aswell but not adjusted

---
+++ Source/SlavicHuntress_BBM/FoeBase.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FoeBase.h"
+
+// Sets default values
+AFoeBase::AFoeBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	mesh = CreateOptionalDefaultSubobject<USkeletalMeshComponent>("Mesh");
+	if (mesh)
+	{
+		mesh->AlwaysLoadOnClient = true;
+		mesh->AlwaysLoadOnServer = true;
+		mesh->bOwnerNoSee = false;
+		mesh->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::AlwaysTickPose;
+		mesh->bCastDynamicShadow = true;
+		mesh->bAffectDynamicIndirectLighting = true;
+		mesh->PrimaryComponentTick.TickGroup = TG_PrePhysics;
+		mesh->SetupAttachment(RootComponent);
+		static FName MeshCollisionProfileName(TEXT("FoeMesh"));
+		mesh->SetCollisionProfileName(MeshCollisionProfileName);
+		mesh->SetGenerateOverlapEvents(true);
+		mesh->SetCanEverAffectNavigation(false);
+	}
+}
+
+// Called when the game starts or when spawned
+void AFoeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	currentHealth = maxHealth;
+	
+}
+
+// Called every frame
+void AFoeBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
