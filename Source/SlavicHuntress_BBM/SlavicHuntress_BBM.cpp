ece6d99f5b2bfc40c94ec8936efd3807ca8e5935
Lee E <leeellermann@blueberrymountain.de>
2023-08-08 10:18:36 +0200

    added repository

---
+++ Source/SlavicHuntress_BBM/SlavicHuntress_BBM.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SlavicHuntress_BBM.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SlavicHuntress_BBM, "SlavicHuntress_BBM" );
---
+++ Source/SlavicHuntress_BBM/SlavicHuntress_BBMGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SlavicHuntress_BBMGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLAVICHUNTRESS_BBM_API ASlavicHuntress_BBMGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
